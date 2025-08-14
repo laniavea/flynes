0f0d79c67f5ebd68b02b932822443c8401c5ba91
laniavea <laniavea@yahoo.com>
2025-08-14 13:12:08 +0300

    Snake game for CPU tests

---
+++ external/snake-game/README.md
@@ -0,0 +1,12 @@
+# Snake on NES (6502)
+
+Wkjagt's [snake game](https://gist.github.com/wkjagt/9043907) on flynes CPU emulator (6502). Project uses SDL to display game's visual.
+
+## How to run
+1. Clone repo
+2. Clone better assertions to the same dir
+3. Install SDL-related packages to your system
+4. Cargo run
+
+## Outro
+Special thanks to bugzmanov for his [book](https://github.com/bugzmanov/nes_ebook) and guide how to display this game. Also thanks to wkjagt for creating a snake game on 6502's asm.
---
+++ external/snake-game/src/main.rs
@@ -0,0 +1,158 @@
+use rand::Rng;
+
+use sdl2::event::Event;
+use sdl2::EventPump;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+
+use flynes::cpu::Cpu;
+use flynes::memory::Memory;
+
+use std::thread;
+use std::time::Duration;
+
+const SNAKE_GAME_WIDTH: u32 = 32;
+const SNAKE_GAME_HEIGHT: u32 = 32;
+const SNAKE_GAME_WIDTH_F32: f32 = 32.0;
+const SNAKE_GAME_HEIGHT_F32: f32 = 32.0;
+const SCALE_WIDTH: f32 = 30.0;
+const SCALE_HEIGHT: f32 = 30.0;
+const ROM_START: usize = 0x0600;
+
+fn main() {
+    pretty_env_logger::init();
+
+    let snake_game_hex: Vec<u8> = vec![
+        0x20, 0x06, 0x06, 0x20, 0x38, 0x06, 0x20, 0x0d, 0x06, 0x20, 0x2a, 0x06, 0x60, 0xa9, 0x02, 0x85,
+        0x02, 0xa9, 0x04, 0x85, 0x03, 0xa9, 0x11, 0x85, 0x10, 0xa9, 0x10, 0x85, 0x12, 0xa9, 0x0f, 0x85,
+        0x14, 0xa9, 0x04, 0x85, 0x11, 0x85, 0x13, 0x85, 0x15, 0x60, 0xa5, 0xfe, 0x85, 0x00, 0xa5, 0xfe,
+        0x29, 0x03, 0x18, 0x69, 0x02, 0x85, 0x01, 0x60, 0x20, 0x4d, 0x06, 0x20, 0x8d, 0x06, 0x20, 0xc3,
+        0x06, 0x20, 0x19, 0x07, 0x20, 0x20, 0x07, 0x20, 0x2d, 0x07, 0x4c, 0x38, 0x06, 0xa5, 0xff, 0xc9,
+        0x77, 0xf0, 0x0d, 0xc9, 0x64, 0xf0, 0x14, 0xc9, 0x73, 0xf0, 0x1b, 0xc9, 0x61, 0xf0, 0x22, 0x60,
+        0xa9, 0x04, 0x24, 0x02, 0xd0, 0x26, 0xa9, 0x01, 0x85, 0x02, 0x60, 0xa9, 0x08, 0x24, 0x02, 0xd0,
+        0x1b, 0xa9, 0x02, 0x85, 0x02, 0x60, 0xa9, 0x01, 0x24, 0x02, 0xd0, 0x10, 0xa9, 0x04, 0x85, 0x02,
+        0x60, 0xa9, 0x02, 0x24, 0x02, 0xd0, 0x05, 0xa9, 0x08, 0x85, 0x02, 0x60, 0x60, 0x20, 0x94, 0x06,
+        0x20, 0xa8, 0x06, 0x60, 0xa5, 0x00, 0xc5, 0x10, 0xd0, 0x0d, 0xa5, 0x01, 0xc5, 0x11, 0xd0, 0x07,
+        0xe6, 0x03, 0xe6, 0x03, 0x20, 0x2a, 0x06, 0x60, 0xa2, 0x02, 0xb5, 0x10, 0xc5, 0x10, 0xd0, 0x06,
+        0xb5, 0x11, 0xc5, 0x11, 0xf0, 0x09, 0xe8, 0xe8, 0xe4, 0x03, 0xf0, 0x06, 0x4c, 0xaa, 0x06, 0x4c,
+        0x35, 0x07, 0x60, 0xa6, 0x03, 0xca, 0x8a, 0xb5, 0x10, 0x95, 0x12, 0xca, 0x10, 0xf9, 0xa5, 0x02,
+        0x4a, 0xb0, 0x09, 0x4a, 0xb0, 0x19, 0x4a, 0xb0, 0x1f, 0x4a, 0xb0, 0x2f, 0xa5, 0x10, 0x38, 0xe9,
+        0x20, 0x85, 0x10, 0x90, 0x01, 0x60, 0xc6, 0x11, 0xa9, 0x01, 0xc5, 0x11, 0xf0, 0x28, 0x60, 0xe6,
+        0x10, 0xa9, 0x1f, 0x24, 0x10, 0xf0, 0x1f, 0x60, 0xa5, 0x10, 0x18, 0x69, 0x20, 0x85, 0x10, 0xb0,
+        0x01, 0x60, 0xe6, 0x11, 0xa9, 0x06, 0xc5, 0x11, 0xf0, 0x0c, 0x60, 0xc6, 0x10, 0xa5, 0x10, 0x29,
+        0x1f, 0xc9, 0x1f, 0xf0, 0x01, 0x60, 0x4c, 0x35, 0x07, 0xa0, 0x00, 0xa5, 0xfe, 0x91, 0x00, 0x60,
+        0xa6, 0x03, 0xa9, 0x00, 0x81, 0x10, 0xa2, 0x00, 0xa9, 0x01, 0x81, 0x10, 0x60, 0xa2, 0x00, 0xea,
+        0xea, 0xca, 0xd0, 0xfb, 0x60
+    ];
+
+    let mut cpu_unit = Cpu::default();
+    let mut memory = Memory::default();
+
+    for (now_shift, now_byte) in snake_game_hex.iter().enumerate() {
+        *memory.get_mut_8bit_value(ROM_START + now_shift) = *now_byte;
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window(
+            "Snake game",
+            (SNAKE_GAME_WIDTH_F32 * SCALE_WIDTH) as u32,
+            (SNAKE_GAME_HEIGHT_F32 * SCALE_HEIGHT) as u32
+        )
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_scale(SCALE_WIDTH, SCALE_HEIGHT).unwrap();
+
+    let creator = canvas.texture_creator();
+    let mut texture = creator
+        .create_texture_target(PixelFormatEnum::RGB24, SNAKE_GAME_WIDTH, SNAKE_GAME_HEIGHT).unwrap();
+
+    
+    let mut screen_state = [0u8; (SNAKE_GAME_WIDTH * 3 * SNAKE_GAME_HEIGHT) as usize];
+    let mut rng = rand::rng();
+
+    cpu_unit.set_pc(ROM_START as u16);
+
+    loop {
+        handle_user_input(&mut memory, &mut event_pump);
+        *memory.get_mut_8bit_value(0xFEusize) = rng.random_range(1..16);
+
+        if read_screen_state(&memory, &mut screen_state) {
+            texture.update(None, &screen_state, 32 * 3).unwrap();
+            canvas.copy(&texture, None, None).unwrap();
+            canvas.present();
+        }
+
+        if cpu_unit.execute_cpu_iteration(&mut memory).is_err() {
+            println!("Unknonwn op");
+            break
+        }
+
+        if cpu_unit.program_counter() as usize == ROM_START + snake_game_hex.len() {
+            println!("Game end");
+            break;
+        }
+
+        thread::sleep(Duration::new(0, 30_000));
+    }
+}
+
+fn handle_user_input(mem: &mut Memory, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                std::process::exit(0)
+            },
+            Event::KeyDown { keycode: Some(Keycode::W), .. } => {
+                *mem.get_mut_8bit_value(0xFFusize) = 0x77;
+            },
+            Event::KeyDown { keycode: Some(Keycode::S), .. } => {
+                *mem.get_mut_8bit_value(0xFFusize) = 0x73;
+            },
+            Event::KeyDown { keycode: Some(Keycode::A), .. } => {
+                *mem.get_mut_8bit_value(0xFFusize) = 0x61;
+            },
+            Event::KeyDown { keycode: Some(Keycode::D), .. } => {
+                *mem.get_mut_8bit_value(0xFFusize) = 0x64;
+            }
+            _ => {/* do nothing */}
+        }
+    }
+}
+
+fn color(byte: u8) -> Color {
+    match byte {
+        0 => sdl2::pixels::Color::BLACK,
+        1 => sdl2::pixels::Color::WHITE,
+        2 | 9 => sdl2::pixels::Color::GREY,
+        3 | 10 => sdl2::pixels::Color::RED,
+        4 | 11 => sdl2::pixels::Color::GREEN,
+        5 | 12 => sdl2::pixels::Color::BLUE,
+        6 | 13 => sdl2::pixels::Color::MAGENTA,
+        7 | 14 => sdl2::pixels::Color::YELLOW,
+        _ => sdl2::pixels::Color::CYAN,
+    }
+}
+
+fn read_screen_state(memory: &Memory, frame: &mut [u8; (SNAKE_GAME_WIDTH * 3 * SNAKE_GAME_HEIGHT) as usize]) -> bool {
+    let mut frame_idx = 0;
+    let mut update = false;
+    for i in 0x0200..0x0600 {
+        let color_idx = memory.get_8bit_value(i as u16);
+        let (b1, b2, b3) = color(color_idx).rgb();
+        if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
+            frame[frame_idx] = b1;
+            frame[frame_idx + 1] = b2;
+            frame[frame_idx + 2] = b3;
+            update = true;
+        }
+        frame_idx += 3;
+    }
+    update
+}
