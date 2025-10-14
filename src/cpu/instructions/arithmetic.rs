f05f3c5d2aec7fe77de8521632b68e11fa804fdc
laniavea <laniavea@yahoo.com>
2025-10-14 19:41:11 +0300

    Arithmetic test update due bus implementation

--- src/cpu/instructions/arithmetic.rs
+++ src/cpu/instructions/arithmetic.rs
@@ -86,155 +86,166 @@ impl Cpu {
     }
 }
 
-// #[test]
-// fn test_arithmetic() {
-//     use rand::{SeedableRng, Rng};
-//     use rand::rngs::StdRng;
-//
-//     use crate::cpu::instructions::shared_ops::is_flag_set;
-//     use crate::cpu::{ZERO_FLAG, NEGATIVE_FLAG};
-//
-//     let mut rng: StdRng = StdRng::seed_from_u64(42);
-//
-//     let mut cpu = Cpu {
-//         reg_a: 0,
-//         cpu_status: 0b0000_0000,
-//         ..Default::default()
-//     };
-//
-//     // ASM code for tests below
-//     // ----------------------
-//     // LDA #$90
-//     // ADC #$90
-//     // ADC #$20
-//     // ADC #$50
-//     // ADC #$6F
-//     // ADC #$FF
-//     // ADC #$7F
-//     // ADC #$00
-//     // ADC #$80
-//     // LDA #$7F
-//     // CLC
-//     // ADC #$01
-//     // ADC #$FF
-//
-//     cpu.reg_a = 0x90;
-//     cpu.op_adc(&0x90);
-//     check_arith(cpu.reg_a, 0x20, cpu.cpu_status, [false, true, false, true]);
-//     cpu.op_adc(&0x20);
-//     check_arith(cpu.reg_a, 0x41, cpu.cpu_status, [false, false, false, false]);
-//     cpu.op_adc(&0x50);
-//     check_arith(cpu.reg_a, 0x91, cpu.cpu_status, [true, true, false, false]);
-//     cpu.op_adc(&0x6F);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     cpu.op_adc(&0xFF);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     cpu.op_adc(&0x7F);
-//     check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, true, false, false]);
-//     cpu.op_adc(&0x00);
-//     check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, false, false, false]);
-//     cpu.op_adc(&0x80);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, true, true, true]);
-//     cpu.reg_a = 0x7F;
-//     set_flag(&mut cpu.cpu_status, CARRY_FLAG, false);
-//     cpu.op_adc(&0x01);
-//     check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, true, false, false]);
-//     cpu.op_adc(&0xFF);
-//     check_arith(cpu.reg_a, 0x7F, cpu.cpu_status, [false, true, false, true]);
-//
-//
-//     // ASM code for tests below
-//     // -----------------------
-//     // LDA #$0
-//     // SBC #$64
-//     // SBC #$9B
-//     // SBC #$01
-//     // SBC #$FC
-//     // SBC #$02
-//     // SBC #$80
-//     // SBC #$80
-//     // SBC #$FF
-//     // SBC #$FD
-//     // SBC #$00
-//     // SBC #$FF
-//     // SBC #$00
-//     // SBC #$00
-//     // CLC
-//     // SBC #$01
-//
-//     cpu.cpu_status = 0b0000_0000;
-//     cpu.reg_a = 0x0;
-//     cpu.op_sbc(&0x64);
-//     check_arith(cpu.reg_a, 0x9B, cpu.cpu_status, [true, false, false, false]);
-//     cpu.op_sbc(&0x9B);
-//     check_arith(cpu.reg_a, 0xFF, cpu.cpu_status, [true, false, false, false]);
-//     cpu.op_sbc(&0x01);
-//     check_arith(cpu.reg_a, 0xFD, cpu.cpu_status, [true, false, false, true]);
-//     cpu.op_sbc(&0xFC);
-//     check_arith(cpu.reg_a, 0x01, cpu.cpu_status, [false, false, false, true]);
-//     cpu.op_sbc(&0x02);
-//     check_arith(cpu.reg_a, 0xFF, cpu.cpu_status, [true, false, false, false]);
-//     cpu.op_sbc(&0x80);
-//     check_arith(cpu.reg_a, 0x7E, cpu.cpu_status, [false, false, false, true]);
-//     cpu.op_sbc(&0x80);
-//     check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, true, false, false]);
-//     cpu.op_sbc(&0xFF);
-//     check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, false, false, false]);
-//     cpu.op_sbc(&0xFD);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     cpu.op_sbc(&0x00);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     cpu.op_sbc(&0xFF);
-//     check_arith(cpu.reg_a, 0x01, cpu.cpu_status, [false, false, false, false]);
-//     cpu.op_sbc(&0x00);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     cpu.op_sbc(&0x00);
-//     check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
-//     set_flag(&mut cpu.cpu_status, CARRY_FLAG, false);
-//     cpu.op_sbc(&0x01);
-//     check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, false, false, false]);
-//
-//     cpu.reg_a = 0x80;
-//     set_flag(&mut cpu.cpu_status, CARRY_FLAG, true);
-//     cpu.op_sbc(&0x7F);
-//
-//     assert_eq!(cpu.reg_a, 0x01);
-//     assert!(is_flag_set(&cpu.cpu_status, OVERFLOW_FLAG));
-//
-//     let mut last_val = rng.random::<u8>();
-//     for _ in 0..1000 {
-//         cpu.reg_a = last_val;
-//         cpu.reg_x = last_val;
-//         cpu.reg_y = last_val;
-//
-//         let random_v = rng.random::<u8>();
-//         let random_st = rng.random::<u8>();
-//
-//         cpu.cpu_status = random_st;
-//         cpu.op_cmp(&random_v);
-//         let new_cpu_status = cpu.cpu_status;
-//
-//         cpu.cpu_status = random_st;
-//         cpu.op_cpx(&random_v);
-//         assert_eq!(cpu.cpu_status, new_cpu_status);
-//
-//         cpu.cpu_status = random_st;
-//         cpu.op_cpy(&random_v);
-//         assert_eq!(cpu.cpu_status, new_cpu_status);
-//
-//         assert_eq!(is_flag_set(&cpu.cpu_status, NEGATIVE_FLAG), last_val.wrapping_sub(random_v) & 0b1000_0000 != 0);
-//         assert_eq!(is_flag_set(&cpu.cpu_status, CARRY_FLAG), last_val >= random_v);
-//         assert_eq!(is_flag_set(&cpu.cpu_status, ZERO_FLAG), last_val == random_v);
-//
-//         last_val = random_v;
-//     }
-//
-//     // flags are NEGATIVE -> OVERFLOW -> ZERO -> CARRY
-//     fn check_arith(res: u8, exp_res: u8, cpu_status: u8, flags: [bool; 4]) {
-//         assert_eq!(res, exp_res);
-//         assert_eq!(is_flag_set(&cpu_status, NEGATIVE_FLAG), flags[0]);
-//         assert_eq!(is_flag_set(&cpu_status, OVERFLOW_FLAG), flags[1]);
-//         assert_eq!(is_flag_set(&cpu_status, ZERO_FLAG), flags[2]);
-//         assert_eq!(is_flag_set(&cpu_status, CARRY_FLAG), flags[3]);
-//     }
-// }
+#[test]
+fn test_arithmetic() {
+    use rand::{SeedableRng, Rng};
+    use rand::rngs::StdRng;
+
+    use crate::cpu::instructions::shared_ops::is_flag_set;
+    use crate::cpu::{ZERO_FLAG, NEGATIVE_FLAG};
+
+    let mut rng: StdRng = StdRng::seed_from_u64(42);
+
+    let mut cpu = Cpu {
+        reg_a: 0,
+        cpu_status: 0b0000_0000,
+        ..Default::default()
+    };
+    let mut bus = Bus::default();
+
+    // ASM code for tests below
+    // ----------------------
+    // LDA #$90
+    // ADC #$90
+    // ADC #$20
+    // ADC #$50
+    // ADC #$6F
+    // ADC #$FF
+    // ADC #$7F
+    // ADC #$00
+    // ADC #$80
+    // LDA #$7F
+    // CLC
+    // ADC #$01
+    // ADC #$FF
+    let adc_values: [u8; 10] = [0x90, 0x20, 0x50, 0x6F, 0xFF, 0x7F, 0x00, 0x80, 0x01, 0xFF];
+    for (now_value_id, now_value) in adc_values.iter().enumerate() {
+        bus.write_8bit_cpu(now_value_id as u16, *now_value);
+    }
+
+    cpu.reg_a = 0x90;
+    cpu.op_adc(&mut bus, 0x0000);
+    check_arith(cpu.reg_a, 0x20, cpu.cpu_status, [false, true, false, true]);
+    cpu.op_adc(&mut bus, 0x0001);
+    check_arith(cpu.reg_a, 0x41, cpu.cpu_status, [false, false, false, false]);
+    cpu.op_adc(&mut bus, 0x0002);
+    check_arith(cpu.reg_a, 0x91, cpu.cpu_status, [true, true, false, false]);
+    cpu.op_adc(&mut bus, 0x0003);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    cpu.op_adc(&mut bus, 0x0004);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    cpu.op_adc(&mut bus, 0x0005);
+    check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, true, false, false]);
+    cpu.op_adc(&mut bus, 0x0006);
+    check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, false, false, false]);
+    cpu.op_adc(&mut bus, 0x0007);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, true, true, true]);
+    cpu.reg_a = 0x7F;
+    set_flag(&mut cpu.cpu_status, CARRY_FLAG, false);
+    cpu.op_adc(&mut bus, 0x0008);
+    check_arith(cpu.reg_a, 0x80, cpu.cpu_status, [true, true, false, false]);
+    cpu.op_adc(&mut bus, 0x0009);
+    check_arith(cpu.reg_a, 0x7F, cpu.cpu_status, [false, true, false, true]);
+
+
+    // ASM code for tests below
+    // -----------------------
+    // LDA #$0
+    // SBC #$64
+    // SBC #$9B
+    // SBC #$01
+    // SBC #$FC
+    // SBC #$02
+    // SBC #$80
+    // SBC #$80
+    // SBC #$FF
+    // SBC #$FD
+    // SBC #$00
+    // SBC #$FF
+    // SBC #$00
+    // SBC #$00
+    // CLC
+    // SBC #$01
+
+    let sbc_values: [u8; 15] = [0x64, 0x9B, 0x01, 0xFC, 0x02, 0x80, 0x80, 0xFF, 0xFD, 0x00, 0xFF, 0x00, 0x00, 0x01, 0x7F];
+    for (now_value_id, now_value) in sbc_values.iter().enumerate() {
+        bus.write_8bit_cpu(now_value_id as u16, *now_value);
+    }
+    cpu.cpu_status = 0b0000_0000;
+    cpu.reg_a = 0x0;
+    cpu.op_sbc(&mut bus, 0x0000);
+    check_arith(cpu.reg_a, 0x9B, cpu.cpu_status, [true, false, false, false]);
+    cpu.op_sbc(&mut bus, 0x0001);
+    check_arith(cpu.reg_a, 0xFF, cpu.cpu_status, [true, false, false, false]);
+    cpu.op_sbc(&mut bus, 0x0002);
+    check_arith(cpu.reg_a, 0xFD, cpu.cpu_status, [true, false, false, true]);
+    cpu.op_sbc(&mut bus, 0x0003);
+    check_arith(cpu.reg_a, 0x01, cpu.cpu_status, [false, false, false, true]);
+    cpu.op_sbc(&mut bus, 0x0004);
+    check_arith(cpu.reg_a, 0xFF, cpu.cpu_status, [true, false, false, false]);
+    cpu.op_sbc(&mut bus, 0x0005);
+    check_arith(cpu.reg_a, 0x7E, cpu.cpu_status, [false, false, false, true]);
+    cpu.op_sbc(&mut bus, 0x0006);
+    check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, true, false, false]);
+    cpu.op_sbc(&mut bus, 0x0007);
+    check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, false, false, false]);
+    cpu.op_sbc(&mut bus, 0x0008);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    cpu.op_sbc(&mut bus, 0x0009);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    cpu.op_sbc(&mut bus, 0x000A);
+    check_arith(cpu.reg_a, 0x01, cpu.cpu_status, [false, false, false, false]);
+    cpu.op_sbc(&mut bus, 0x000B);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    cpu.op_sbc(&mut bus, 0x000C);
+    check_arith(cpu.reg_a, 0x00, cpu.cpu_status, [false, false, true, true]);
+    set_flag(&mut cpu.cpu_status, CARRY_FLAG, false);
+    cpu.op_sbc(&mut bus, 0x000D);
+    check_arith(cpu.reg_a, 0xFE, cpu.cpu_status, [true, false, false, false]);
+
+    cpu.reg_a = 0x80;
+    set_flag(&mut cpu.cpu_status, CARRY_FLAG, true);
+    cpu.op_sbc(&mut bus, 0x000E);
+
+    assert_eq!(cpu.reg_a, 0x01);
+    assert!(is_flag_set(&cpu.cpu_status, OVERFLOW_FLAG));
+
+    let mut last_val = rng.random::<u8>();
+    for _ in 0..1000 {
+        cpu.reg_a = last_val;
+        cpu.reg_x = last_val;
+        cpu.reg_y = last_val;
+
+        let random_v = rng.random::<u8>();
+        let random_st = rng.random::<u8>();
+
+        bus.write_8bit_cpu(0x0000u16, random_v);
+
+        cpu.cpu_status = random_st;
+        cpu.op_cmp(&mut bus, 0x0000);
+        let new_cpu_status = cpu.cpu_status;
+
+        cpu.cpu_status = random_st;
+        cpu.op_cpx(&mut bus, 0x0000);
+        assert_eq!(cpu.cpu_status, new_cpu_status);
+
+        cpu.cpu_status = random_st;
+        cpu.op_cpy(&mut bus, 0x0000);
+        assert_eq!(cpu.cpu_status, new_cpu_status);
+
+        assert_eq!(is_flag_set(&cpu.cpu_status, NEGATIVE_FLAG), last_val.wrapping_sub(random_v) & 0b1000_0000 != 0);
+        assert_eq!(is_flag_set(&cpu.cpu_status, CARRY_FLAG), last_val >= random_v);
+        assert_eq!(is_flag_set(&cpu.cpu_status, ZERO_FLAG), last_val == random_v);
+
+        last_val = random_v;
+    }
+
+    // flags are NEGATIVE -> OVERFLOW -> ZERO -> CARRY
+    fn check_arith(res: u8, exp_res: u8, cpu_status: u8, flags: [bool; 4]) {
+        assert_eq!(res, exp_res);
+        assert_eq!(is_flag_set(&cpu_status, NEGATIVE_FLAG), flags[0]);
+        assert_eq!(is_flag_set(&cpu_status, OVERFLOW_FLAG), flags[1]);
+        assert_eq!(is_flag_set(&cpu_status, ZERO_FLAG), flags[2]);
+        assert_eq!(is_flag_set(&cpu_status, CARRY_FLAG), flags[3]);
+    }
+}
